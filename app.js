6c0b1400f93b1e63405e286a2dde74c25b249aeb
jeric-basco22 <125936179+jeric-basco22@users.noreply.github.com>
2023-03-01 12:27:17 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const yesbtn = document.querySelector('.button-yes');
+const nobtn = document.querySelector('.button-no');
+
+const wrapperRect = wrapper.getBoundingClientRect();
+const nobtnRect = nobtn.getBoundingClientRect();
+
+yesbtn.addEventListener('click', () =>{
+    question.innerHTML = 'Ayyyiiiieeehhh😍🥰😜';
+});
+
+nobtn.addEventListener('click', () => {
+    const i = Math.floor(Math.random()*(wrapperRect.width - nobtnRect.width))+1;
+    const j = Math.floor(Math.random()*(wrapperRect.height - nobtnRect.height))+1;
+
+    nobtn.style.left = i + 'px';
+    nobtn.style.left = j + 'px';
+})
